97847d24fba94104bd34b7d2216424f1228208c7
mjc284 <mjc284@cam.ac.uk>
2022-11-23 15:01:34 +0000

    Fixed Karnaugh designer

--- src/karnaugh.py
+++ src/karnaugh.py
@@ -38,41 +38,88 @@ def map(table):
             if((line[1] == 1) or (line[1] == 'X')):
                 tmp += [line]
         selected_maps += [tmp]
-    
+
+    old_processed = [1]
     processed = []
-    for i in range(0, num_inputs):
+    while(old_processed != processed):
         pre_selected_maps = []
+        old_processed = processed
+        processed = []
         for map in selected_maps:
             num_entries = len(map)
             tmp = []
+
             if(num_entries > 2):
-                for index1 in range(1, num_entries - 1):
-                    cnt = 0
+                markoff = []
+                for j in range(0, num_entries):
+                    markoff += [0]
+                for index1 in range(0, num_entries):
                     for index2 in range(index1 + 1, num_entries):
                         neighbor = check_neighbor(map[index1][0], map[index2][0])
+                        
                         if(neighbor[0][0] < 2):
                             if((map[index1][1] == 'X') and (map[index2][1] == 'X')):
                                 tmp += [[neighbor[1], 'X']]
                             else:
                                 tmp += [[neighbor[1], '1']]
-                            cnt += 1
-                    if(cnt == 0):
+                            markoff[index1] += 1
+                            markoff[index2] += 1
+
+                    if(markoff[index1] == 0):
                         tmp += [map[index1]]
             if(tmp != []):
                 processed += [tmp]
 
         selected_maps = []
-        selected_maps += processed
+        selected_maps += processed        
 
+    print(selected_maps)
+    print("")
     mapped = []
+    cnt = 0
     for map in selected_maps:
         tmp = []
+        tmp += [[table[0][0], table[0][1][cnt]]]
         for line in map:
             if(line[1] != 'X'):
                 tmp += [line]
+        cnt += 1
         mapped += [tmp]
 
+    print(mapped)
+
     return mapped
+
+def present(mapped):
+    for map in mapped:
+        label = 1
+        swc2 = 0
+        print(map[0][1], end = '')
+        print(" = ", end = '')
+        for line in map:
+            if(swc2 == 1):
+                    print(" | ", end = '')
+            if(label == 0):
+                swc1 = 0
+                num_item = len(line[0])
+                for index in range(0, num_item):
+                    if(line[0][index] == 1):
+                        if(swc1 == 1):
+                            print("&", end = '')
+                        print(map[0][0][index], end = '')
+                        swc1 = 1
+                    elif(line[0][index] == 0):
+                        if(swc1 == 1):
+                            print("&", end = '')
+                        print("!", end = '')
+                        print(map[0][0][index], end = '')
+                        swc1 = 1
+                swc2 = 1
+            label = 0
+        print("")
+        
+
+
 
 
    
--- src/script.py
+++ src/script.py
@@ -3,7 +3,33 @@ import states_parser as sp
 import karnaugh as kn
 
 states = np.array([[0, 1], [1, 2], [2, 0]])
+states = np.array([[1, 1, 1, 3], [2, 2, 2, 2], [4, 4, 4, 4], [4, 4, 4, 4], [0, 0, 0, 0]])
 parsed = sp.parse(states)
+sp.present(parsed)
+
+
+parsed = [[['A', 'B', 'C', 'D'], ['O']],
+[[0, 0, 0, 0], [0]],
+[[0, 0, 0, 1], [0]],
+[[0, 0, 1, 0], [1]],
+[[0, 0, 1, 1], [1]],
+[[0, 1, 0, 0], [0]],
+[[0, 1, 0, 1], [1]],
+[[0, 1, 1, 0], [0]],
+[[0, 1, 1, 1], [1]],
+[[1, 0, 0, 0], [1]],
+[[1, 0, 0, 1], [0]],
+[[1, 0, 1, 0], [0]],
+[[1, 0, 1, 1], [0]],
+[[1, 1, 0, 0], [0]],
+[[1, 1, 0, 1], [1]],
+[[1, 1, 1, 0], [0]],
+[[1, 1, 1, 1], [1]]]
+
+for i in parsed:
+    print(i)
+
+print("")
 
 processed = kn.map(parsed)
-print(processed)
+kn.present(processed)
